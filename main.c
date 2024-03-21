1f94929aaec226afb5b1c0c7a1403e66a79d02c3
anushi2511 <anushi05.ag@gmail.com>
2024-03-21 23:29:03 +0530

    new file:   .vscode/tasks.json 	modified:   main.c 	new file:   main.exe

--- main.c
+++ main.c
@@ -14,5 +14,26 @@ int binary_search(int arr[], int left, int right, int key) {
 }
 
 int main() {
+    int n;
+    
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d sorted elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int key;
+    
+    scanf("%d", &key);
+
+    int result = binary_search(arr, 0, n - 1, key);
+    if (result != -1)
+        printf("Element %d found at index %d\n", key, result);
+    else
+        printf("Element %d not found in the array\n", key);
+
+    return 0;
+
 
 }
